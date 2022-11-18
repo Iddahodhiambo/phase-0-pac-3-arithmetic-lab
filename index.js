20a671662777e50c5f2c093f92c549e3b391cfc0
Iddahodhiambo <iddahawuor@yahoo.com>
2022-11-18 13:25:51 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,71 @@
+var a, b
+function add() {
+
+}
+add();
+
+function subtract() {
+
+}
+subtract();
+
+function multiply() {
+
+}
+multiply();
+
+function divide() {
+
+}
+multiply();
+ 
+function add(a,b) {
+    return(a+ b)
+ }
+ add(10,5);
+ function subtract(a,b) {
+    return(a-b)
+ }
+ subtract(10,5);
+ function multiply(a,b) {
+    return(a * b)
+ }
+ multiply(10,5);
+ function divide(a,b) {
+    return(a / b)
+ }
+ divide(10,5);
+
+ function increment(a) {
+    return(a + 1)
+
+ }
+ increment(10);
+
+
+function decrement(a) {
+    return(a -1)
+
+}
+decrement(10);
+
+function makeInt(a) {
+    return parseInt(a);
+}
+makeInt(10);
+function makeInt(a) {
+   return parseInt(a, 10);
+}
+makeInt(10);
+
+function makeInt(a) {
+   return parseInt(a,10);
+}
+makeInt(a);
+
+ function preserveDecimal(n) {
+   return(parseFloat(n))
+ }
+
+ preserveDecimal(2.222);
+
